9511263d0a5780c28e62846a9ccaa71674657d68
SYK <61903127+LapinRobin@users.noreply.github.com>
2022-11-14 11:17:14 +0100

    delete build

--- Yikang-Wiki/assets/js/e8fcdde7.c949b126.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkyikang_wiki=self.webpackChunkyikang_wiki||[]).push([[5792],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
